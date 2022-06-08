aede0ae9c945ef21ca049808537609008a294d39
Quinzy Felthauser <qfelthauser@my.apsu.edu>
2022-06-08 10:32:59 +0000

    password issues

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -105,7 +105,7 @@ def get_dealer_details(request, dealer_id):
     context = {}
     if request.method == "GET":
         url = 'https://quincy.mybluemix.net/'
-        # apikey= Add API key here
+        apikey="86LCWp_sISZS-VSr_bsvzVZ7gvcoWdmqFw1q7JvnpM5H"
         context = {"reviews":  restapis.get_dealer_reviews_by_id_from_cf(url, dealer_id)}
         return render(request, 'djangoapp/dealer_details.html', context)
 
