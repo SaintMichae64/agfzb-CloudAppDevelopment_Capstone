ff6d3d96b37b0237c296f91604867a251c142919
SaintMichae64 <71401539+SaintMichae64@users.noreply.github.com>
2022-06-17 16:42:26 -0500

    Update models.py
    instructor corrections

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,81 +1,108 @@
 from django.db import models
+from django.core import serializers
 from django.utils.timezone import now
+import uuid
+import json
+
+# Create your models here.
+
+# <HINT> Create a Car Make model `class CarMake(models.Model)`:
+# - Name
+# - Description
+# - Any othere fields you would like to include in car make model
+# - __str__ method to print a car make object
 
 
-# Car Make model `class CarMake(models.Model)`:
 class CarMake(models.Model):
-    name = models.CharField(null=False, max_length=30, default='Honda')
-    description = models.CharField(max_length=280, default='Honda cars are awesome')
+    name = models.CharField(null=False, max_length=100, default='Make')
+    description = models.CharField(max_length=500)
 
     def __str__(self):
-        return self.name
+        return "Name: " + self.name
 
-# Car Model model `class CarModel(models.Model):`:
+# <HINT> Create a Car Model model `class CarModel(models.Model):`:
+# - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
+# - Name
+# - Type (CharField with a choices argument to provide limited choices such as Sedan, SUV, WAGON, etc.)
+# - Year (DateField)
+# - Any other fields you would like to include in car make model
+# - __str__ method to print a car make object
+
+
 class CarModel(models.Model):
-    # Type (CharField with a choices argument to provide limited choices)    
-    TYPES = (
-            ("SEDAN", "Sedan"), ("SUV", "SUV"), ("TRUCK", "Truck"), ("SPORTS CAR", "Sports Car")
-            )
-    
-    # Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
+    id = models.IntegerField(default=1,primary_key=True)
+  name = models.CharField(null=False, max_length=100, default='Car')
+   
+    SEDAN = 'Sedan'
+    SUV = 'SUV'
+    WAGON = 'Wagon'
+    MINIVAN = 'Minivan'
+    CAR_TYPES = [
+        (SEDAN, 'Sedan'),
+        (SUV, 'SUV'),
+        (WAGON, 'Wagon'),
+        (MINIVAN, 'Minivan')
+    ]
+
+    type = models.CharField(
+        null=False,
+        max_length=50,
+        choices=CAR_TYPES,
+        default=SEDAN
+    )
     make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
-    name = models.CharField(null=False, max_length=30)
-    #constant for c_type choices
-    c_type = models.CharField(max_length=30, choices=TYPES)
-    # Dealer id, used to refer a dealer created in cloudant database
-    dealer_id = models.IntegerField()
-    year = models.DateField()
-    # __str__ method to print a car make object
+    year = models.DateField(default=now)
+
     def __str__(self):
-        return "Name: " + self.name + \
-                " Make Name: "+ self.make.name + \
-                " Type: " + self.c_type + \
-                " Dealer ID: " + str(self.dealer_id)+ \
-                " Year: " + str(self.year)
-                
+        return "Name: " + self.name
 
 
-# Python class `CarDealer` to hold dealer data
 class CarDealer:
 
-    def __init__(self, address, city, full_name, id, lat, long, short_name, st, zip):
+    def __init__(self, address, city, id, lat, long, st, zip, full_name):
         # Dealer address
         self.address = address
         # Dealer city
         self.city = city
-        # Dealer Full Name
-        self.full_name = full_name
-        # Dealer id
+        
+         # Dealer id
         self.id = id
         # Location lat
         self.lat = lat
         # Location long
         self.long = long
-        # Dealer short name
-        self.short_name = short_name
+
         # Dealer state
         self.st = st
         # Dealer zip
         self.zip = zip
 
+        # Full name
+        self.full_name = full_name
+
     def __str__(self):
         return "Dealer name: " + self.full_name
 
-# Python class `DealerReview` to hold review data
+
 class DealerReview:
-    def __init__(self, dealership, name, purchase, review, purchase_date, car_make, car_model, car_year,sentiment, id):
-        self.dealership=dealership
-        self.name=name
-        self.purchase=purchase
-        self.review=review
-        self.purchase_date=purchase_date
-        self.car_make=car_make
-        self.car_model=car_model
-        self.car_year=car_year
-        self.sentiment=sentiment #Watson NLU service
-        self.id=id
+
+    def __init__(self, dealership, name, purchase, review):
+        # Required attributes
+        self.dealership = dealership
+        self.name = name
+        self.purchase = purchase
+        self.review = review
+        # Optional attributes
+        self.purchase_date = ""
+        self.purchase_make = ""
+        self.purchase_model = ""
+        self.purchase_year = ""
+        self.sentiment = ""
+        self.id = ""
 
     def __str__(self):
-        return "Review: " + self.review +\
-                " Sentiment: " + self.sentiment
+        return "Review: " + self.review
 
+    def to_json(self):
+        return json.dumps(self, default=lambda o: o.__dict__,
+                            sort_keys=True, indent=4)
