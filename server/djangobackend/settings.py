60c26330b6accfb493405563afe0794eadf4162e
SaintMichae64 <71401539+SaintMichae64@users.noreply.github.com>
2022-07-12 06:23:11 -0500

    Update settings.py
    corrected Postgresql password

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -133,3 +133,4 @@ MEDIA_ROOT = os.path.join(STATIC_ROOT, 'media')
 MEDIA_URL = '/media/'
 # Configure DEFAULT_AUTO_FIELD to remove error, Configure the DEFAULT_AUTO_FIELD setting or the DjangoappConfig.default_auto_field attribute to point to a subclass of AutoField, e.g. 'django.db.models.BigAutoField'.
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+
