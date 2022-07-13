dabb15368af13f4efcb6037e45241898fd571030
SaintMichae64 <71401539+SaintMichae64@users.noreply.github.com>
2022-07-12 19:33:20 -0500

    Update settings.py
    updated postgresl password, corrected error removing "; also, from user and password...had a single double quote at the end of words  'USER" and 'PASSWORD", changed to '

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -83,8 +83,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
         # will need to un-comment these for running the project with updated user and password
-        'USER": 'b00ndo9761',
-        'PASSWORD":'Mjg0OC1iMDBuZG85',
+        'USER': 'b00ndo9761',
+        'PASSWORD':'NTY2My1iMDBuZG85',
         'HOST':'localhost',
         'PORT':'5432',
     }
