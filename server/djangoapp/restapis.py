e6c8cd17af7b1826b8aae01dc6a2a2519fff5ae4
SaintMichae64 <71401539+SaintMichae64@users.noreply.github.com>
2022-07-14 05:58:08 -0500

    Update restapis.py
    Getting KeyError at Line 50, changed "rows" to "body"; troubleshooting

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -47,7 +47,7 @@ def get_dealerships_from_cf(url, **kwargs):
     json_result = get_request(url)
     if json_result:
         # Get the row list in JSON as dealers
-        dealers = json_result["rows"]
+        dealers = json_result["body"]
         # For each dealer object
         for dealer in dealers:
             # Get its content in `doc` object
