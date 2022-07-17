4b8b1929e8c3287a70820e957149652b2971dc75
SaintMichae64 <71401539+SaintMichae64@users.noreply.github.com>
2022-07-17 08:03:34 -0500

    Update restapis.py
    line 57, was short-name, corrected to full_name to match model.py file

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -54,7 +54,7 @@ def get_all_dealerships_from_cf(url, **kwargs):
             dealer_doc = dealer["doc"]
             # Create a CarDealer object with values in `doc` object
             dealer_obj = CarDealer(address=dealer_doc["address"], city=dealer_doc["city"], id=dealer_doc["id"], lat=dealer_doc["lat"], long=dealer_doc["long"],
-                                   short_name=dealer_doc["short_name"],
+                                   full_name=dealer_doc["full_name"],
                                    st=dealer_doc["st"], zip=dealer_doc["zip"])
             results.append(dealer_obj)
 
