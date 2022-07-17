de0f1fdcf3e1e0e0c91257f063de1d107827c0a4
SaintMichae64 <71401539+SaintMichae64@users.noreply.github.com>
2022-07-16 19:16:40 -0500

    Update views.py
    line 6 still

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render, redirect
 from .models import CarModel, CarMake, CarDealer, DealerReview, ReviewPost
-from .restapis import get_dealerships_from_cf, get_reviews_from_cf, get_request, post_request
+from .restapis import get_all_dealerships_from_cf, get_allReviews_byID_from_cf, get_post_review_from_cf, get_request, post_request
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from datetime import datetime
