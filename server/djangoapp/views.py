9cb898d79243b34fc5ab9248290d15015a73e903
SaintMichae64 <71401539+SaintMichae64@users.noreply.github.com>
2022-08-01 17:41:19 -0500

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -35,7 +35,7 @@ def get_dealership(request):
     if request.method == "GET":
         context = {}
         url = "https://2d6871f8.us-south.apigw.appdomain.cloud/api/dealership"
-        dealerships = dealership(url)
+        dealerships = get_dealership(url)
         context["dealership_list"] = dealerships
         return render(request, 'djangoapp/index.html', context)
 
