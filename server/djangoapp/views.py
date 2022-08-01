0d971d7cdbc67e4ebda048fc115e4169afa9b90a
SaintMichae64 <71401539+SaintMichae64@users.noreply.github.com>
2022-08-01 05:15:06 -0500

    Update views.py
    line 35 changed from get_dealerships to dealership

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -35,7 +35,7 @@ def get_dealership(request):
     if request.method == "GET":
         context = {}
         url = "https://2d6871f8.us-south.apigw.appdomain.cloud/api/dealership"
-        dealerships = get_dealership(url)
+        dealerships = dealership(url)
         context["dealership_list"] = dealerships
         return render(request, 'djangoapp/index.html', context)
 
