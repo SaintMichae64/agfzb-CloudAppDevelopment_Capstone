682500831a16aaaadc949112db01c90a327db073
SaintMichae64 <71401539+SaintMichae64@users.noreply.github.com>
2022-08-02 05:35:40 -0500

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -44,7 +44,7 @@ def get_allReviews_byID(request, id):
     if request.method == "GET":
         context = {}
         dealer_url = "https://2d6871f8.us-south.apigw.appdomain.cloud/api/dealership"
-        dealer = get_allReviews_byID(dealer_url, id=id)
+        dealer = get_dealership(dealer_url, id=id)
         context["dealer"] = dealer
     
         review_url = "https://2d6871f8.us-south.apigw.appdomain.cloud/api/get_review"
