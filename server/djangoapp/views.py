36f08661a1665ec1534db6878f022d983b099be5
SaintMichae64 <71401539+SaintMichae64@users.noreply.github.com>
2022-08-04 19:35:13 -0500

    Update views.py
    line 61 and the entire get_allReviews_byID

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -44,11 +44,11 @@ def get_allReviews_byID(request, id):
     if request.method == "GET":
         context = {}
         dealer_url = "https://2d6871f8.us-south.apigw.appdomain.cloud/api/dealership"
-        dealer = get_dealership(dealer_url, id=id)
+        dealer = get_dealer_by_id_from_cf(dealer_url, id=id)
         context["dealer"] = dealer
     
         review_url = "https://2d6871f8.us-south.apigw.appdomain.cloud/api/get_review"
-        reviews = get_allReviews_byID(review_url, id=id)
+        reviews = get_dealer_reviews_from_cf(review_url, id=id)
         print(reviews)
         context["reviews"] = reviews
         
@@ -58,7 +58,7 @@ def get_allReviews_byID(request, id):
 def add_review(request, id):
     context = {}
     dealer_url = "https://2d6871f8.us-south.apigw.appdomain.cloud/api/dealership"
-    dealer = get_dealership(dealer_url, id=id)
+    dealer = get_dealer_by_id_from_cf(dealer_url, id=id)
     context["dealer"] = dealer
     if request.method == 'GET':
         # Get cars for the dealer
